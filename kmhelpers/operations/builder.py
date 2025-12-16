a7c4ec89e0c70365746a20af9f7529ab75cfadd4
Sébastien Bellenous <sebastien.bellenous@inria.fr>
2025-12-16 11:46:02 +0100

    Add typing in builder

--- kmhelpers/operations/builder.py
+++ kmhelpers/operations/builder.py
@@ -1,27 +1,30 @@
 from ..core import KmindexWrapper, KmtricksIndex, KmindexRegistry, Toolbox
 from ..operations import FofManager
 import os
+from typing import Any
 
 
 class IndexBuilder:
-    def __init__(self, output_index_path):
+    def __init__(self, output_index_path: str) -> None:
         """Initialize the IndexBuilder."""
         self._path = Toolbox.get_canonical_path(output_index_path)
         os.makedirs(self.path, exist_ok=True)
         self._registry = KmindexRegistry(os.path.join(self.path, "registry"))
 
     @property
-    def index(self):
+    def index(self) -> KmindexRegistry:
         return self._registry
 
     @property
-    def path(self):
+    def path(self) -> str:
         return self._path
 
-    def create_subindex(self, name, samples):
+    def create_subindex(self, name: str, samples: dict[str, str]) -> None:
         wrapper = KmindexWrapper()
         fof = FofManager(samples=samples)
         fof_path = os.path.join(self.path, f"{name}.fof")
         fof.save(fof_path=fof_path)
-        wrapper.build(fof_path, self.index.root_path, )
-
+        wrapper.build(
+            fof_path,
+            self.index.root_path,
+        )
