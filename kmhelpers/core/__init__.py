afdf79d8668afac54e32e6df97f88df44de405ef
Sébastien Bellenous <sebastien.bellenous@inria.fr>
2025-12-23 14:22:58 +0100

    Fixing import

--- kmhelpers/core/__init__.py
+++ kmhelpers/core/__init__.py
@@ -3,6 +3,7 @@
 from kmhelpers.core.utils import Main, Bin, Toolbox, Kmindex, BlockCompressorZSTD
 from kmhelpers.core.index import KmtricksIndex, KmindexRegistry
 from kmhelpers.core.wrapper import KmindexWrapper
+from kmhelpers.core.bloom_filter import BloomFilterSpecs
 
 __all__ = [
     "Main",
@@ -13,4 +14,5 @@ __all__ = [
     "KmtricksIndex",
     "KmindexRegistry",
     "KmindexWrapper",
+    "BloomFilterSpecs",
 ]
